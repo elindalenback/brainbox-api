b8dd697e13b23d79046a60027f81818757b08266
elindalenback <elin.dalenback@gmail.com>
2024-04-16 12:09:23 +0000

    Add image validation for folder images

--- notebooks/serializers.py
+++ notebooks/serializers.py
@@ -5,6 +5,19 @@ from notes.serializers import NoteSerializer
 class NotebookSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     notes = NoteSerializer(many=True, read_only=True)
+
+    def validate_folder_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
     
     class Meta:
         model = Notebook
