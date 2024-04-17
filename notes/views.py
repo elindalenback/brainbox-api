491fa745c6da5b6b92bcd7b112c9a04ed762309e
elindalenback <elin.dalenback@gmail.com>
2024-04-17 11:33:13 +0000

    Refactor profiles and notes views

--- notes/views.py
+++ notes/views.py
@@ -1,76 +1,26 @@
-from django.http import Http404
-from django.shortcuts import render
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.apps import apps
+from rest_framework import generics, permissions
 from brainbox_api.permissions import IsOwnerOrReadOnly
-
 from .models import Note
 from .serializers import NoteSerializer
 
 
-class NoteList(APIView):
+class NoteList(generics.ListCreateAPIView):
+    """
+    List notes or create a note if logged in
+    The perform_create method associates the note with the logged in user.
+    """
     serializer_class = NoteSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Note.objects.all()
 
-    def get(self, request):
-        notes = Note.objects.all()
-        serializer = NoteSerializer(
-            notes, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = NoteSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class NoteDetail(APIView):
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a note and edit or delete it if you own it.
+    """
+    serializer_class = NoteSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = NoteSerializer
-
-    def get_object(self, pk):
-        try:
-            note = Note.objects.get(pk=pk)
-            self.check_object_permissions(self.request, note)
-            return note
-        except Note.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(
-            note, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(
-            note, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        note = self.get_object(pk)
-        note.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Note.objects.all()
