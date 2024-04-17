bd466e18e544bedb656b16b1d2547cb284a1e15f
elindalenback <elin.dalenback@gmail.com>
2024-04-17 10:31:03 +0000

    Add detail view for notes and edit & delete functionallity

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.apps import apps
+from brainbox_api.permissions import IsOwnerOrReadOnly
 
 from .models import Note
 from .serializers import NoteSerializer
@@ -32,4 +34,43 @@ class NoteList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class NoteDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = NoteSerializer
+
+    def get_object(self, pk):
+        try:
+            note = Note.objects.get(pk=pk)
+            self.check_object_permissions(self.request, note)
+            return note
+        except Note.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(
+            note, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        note = self.get_object(pk)
+        serializer = NoteSerializer(
+            note, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        note = self.get_object(pk)
+        note.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
