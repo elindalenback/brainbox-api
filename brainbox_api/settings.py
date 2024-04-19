71a595dc0b5e19edef2b6fcae885d67a012f0fef
elindalenback <elin.dalenback@gmail.com>
2024-04-19 18:14:52 +0000

    Debugging: Update CORS settings

--- brainbox_api/settings.py
+++ brainbox_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-elindalenba-brainboxapi-veqs9tw81tl.ws-eu110.gitpod.io', 'brain-box-c3ea3510445f.herokuapp.com',]
+ALLOWED_HOSTS = ['localhost', '8000-elindalenba-brainboxapi-veqs9tw81tl.ws-eu110.gitpod.io', 'brain-box-c3ea3510445f.herokuapp.com',]
 
 # Application definition
 
@@ -125,10 +125,14 @@ TEMPLATES = [
 ]
 
 if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get("CLIENT_ORIGIN"),
+    ]
+else:
+    CORS_ALLOW_ALL_ORIGINS = True  # Temporarily allow all origins for debugging
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.%2A/.gitpod/.io$",
+    r"^https://.*\.gitpod\.io$",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
